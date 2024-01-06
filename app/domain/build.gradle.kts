dd469cdfdbb52564fa249d68a113a823d6beb544
Gazim <gazim1304@gmail.com>
2024-01-06 01:06:16 +0100

    Added other targets for support domain

--- app/domain/build.gradle.kts
+++ app/domain/build.gradle.kts
@@ -1,8 +1,24 @@
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
 }
 
 kotlin {
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+    }
+
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
     jvm()
 
     sourceSets {
@@ -11,5 +27,13 @@ kotlin {
             implementation(libs.kotlinx.coroutines.core)
             implementation(libs.kotlinx.datetime)
         }
+    }
+}
+
+android {
+    namespace = "com.gazim.gmessenger.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
     }
 }
