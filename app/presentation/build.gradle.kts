84bc6a2e0b6e10ed00bc7b05b88c4c535279ba6a
Gazim <gazim1304@gmail.com>
2024-04-02 14:01:16 +0200

    Refactoring

--- app/presentation/build.gradle.kts
+++ app/presentation/build.gradle.kts
@@ -63,7 +63,6 @@ kotlin {
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
             implementation(libs.kotlinx.coroutines.swing)
-            implementation(libs.jSystemThemeDetector)
         }
     }
 }
--- app/presentation/src/androidMain/kotlin/com/gazim/gmessenger/presentation/theme/Theme.android.kt
+++ app/presentation/src/androidMain/kotlin/com/gazim/gmessenger/presentation/theme/Theme.android.kt
@@ -2,5 +2,6 @@ package com.gazim.gmessenger.presentation.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+
 @Composable
 actual fun isSystemInDarkTheme() = isSystemInDarkTheme()
