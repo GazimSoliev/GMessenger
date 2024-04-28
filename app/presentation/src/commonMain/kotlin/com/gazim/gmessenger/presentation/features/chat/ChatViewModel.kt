6cc36d9c5c7fd0dd086f9784193f815b1afb44d0
Gazim <gazim1304@gmail.com>
2024-04-28 10:52:35 +0200

    Added some private modifiers

--- app/presentation/src/commonMain/kotlin/com/gazim/gmessenger/presentation/features/chat/ChatViewModel.kt
+++ app/presentation/src/commonMain/kotlin/com/gazim/gmessenger/presentation/features/chat/ChatViewModel.kt
@@ -26,7 +26,7 @@ import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.syntax.simple.reduce
 
-typealias IntentScope = SimpleSyntax<ChatState, ChatSideEffect>
+private typealias IntentScope = SimpleSyntax<ChatState, ChatSideEffect>
 
 // todo: take out functions
 class ChatViewModel(
--- app/presentation/src/commonMain/kotlin/com/gazim/gmessenger/presentation/features/user/UserViewModel.kt
+++ app/presentation/src/commonMain/kotlin/com/gazim/gmessenger/presentation/features/user/UserViewModel.kt
@@ -21,7 +21,7 @@ import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.syntax.simple.reduce
 
-typealias IntentScope = SimpleSyntax<UserState, UserSideEffect>
+private typealias IntentScope = SimpleSyntax<UserState, UserSideEffect>
 
 // todo: Take out actions
 class UserViewModel(
