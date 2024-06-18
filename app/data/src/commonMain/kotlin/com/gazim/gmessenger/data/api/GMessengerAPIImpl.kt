b9a515b80592fad11f38cfc066ace35ffe33a24e
Gazim <gazim1304@gmail.com>
2024-06-18 09:49:12 +0200

    Added closeable for API

--- app/data/src/commonMain/kotlin/com/gazim/gmessenger/data/api/GMessengerAPIImpl.kt
+++ app/data/src/commonMain/kotlin/com/gazim/gmessenger/data/api/GMessengerAPIImpl.kt
@@ -2,7 +2,6 @@
 
 package com.gazim.gmessenger.data.api
 
-import com.gazim.gmessenger.api.model.IChat
 import com.gazim.gmessenger.data.model.toAPI
 import com.gazim.gmessenger.data.model.toChatWebSocketModel
 import com.gazim.gmessenger.data.model.toDomain
@@ -10,6 +9,7 @@ import com.gazim.gmessenger.data.model.toNotificationWebSocketModel
 import com.gazim.gmessenger.domain.api.GMessengerAPI
 import com.gazim.gmessenger.domain.model.*
 import com.gazim.gmessenger.api.GMessengerAPI as GMAPI
+import com.gazim.gmessenger.api.model.IChat as IChatAPI
 import com.gazim.gmessenger.api.model.User as UserAPI
 
 class GMessengerAPIImpl(
@@ -17,11 +17,11 @@ class GMessengerAPIImpl(
 ) : GMessengerAPI {
     private val gMessengerAPI = GMAPI(token)
 
-    override suspend fun getChats(): List<com.gazim.gmessenger.domain.model.IChat> = gMessengerAPI.getChats().map(IChat::toDomain)
+    override suspend fun getChats(): List<IChat> = gMessengerAPI.getChats().map(IChatAPI::toDomain)
 
     override suspend fun filterUsers(query: String): List<User> = gMessengerAPI.findUser(query).map(UserAPI::toDomain)
 
-    override suspend fun getChat(chatModel: com.gazim.gmessenger.domain.model.IChat): IChatWebSocketModel =
+    override suspend fun getChat(chatModel: IChat): IChatWebSocketModel =
         gMessengerAPI
             .getChatWebSocket(chatModel.toAPI())
             .toChatWebSocketModel((if (chatModel is PrivateChat) chatModel.user.nickname else chatModel.title))
@@ -35,7 +35,7 @@ class GMessengerAPIImpl(
     override suspend fun getNotifications(): INotificationWebSocketModel = gMessengerAPI.getNotifications().toNotificationWebSocketModel()
 
     override suspend fun getMessages(
-        chatModel: com.gazim.gmessenger.domain.model.IChat,
+        chatModel: IChat,
         key: MessagePageKey?,
     ): MessagePage = gMessengerAPI.getMessages(chatModel.toAPI(), key?.toAPI()).toDomain()
 
@@ -47,4 +47,8 @@ class GMessengerAPIImpl(
     ): Image = gMessengerAPI.uploadProfilePhoto(type, bytes).toDomain()
 
     override suspend fun getImageContent(photoId: String): ByteArray = gMessengerAPI.getImageContent(photoId)
+
+    override fun close() {
+        gMessengerAPI.close()
+    }
 }
