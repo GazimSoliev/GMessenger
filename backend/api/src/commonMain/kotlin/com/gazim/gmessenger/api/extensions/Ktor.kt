64fbd65bd5494b327f859ce9762f7f2de10f3391
Gazim <gazim1304@gmail.com>
2024-06-19 00:17:29 +0200

    Saved hot fix

--- backend/api/src/commonMain/kotlin/com/gazim/gmessenger/api/extensions/Ktor.kt
+++ backend/api/src/commonMain/kotlin/com/gazim/gmessenger/api/extensions/Ktor.kt
@@ -1,10 +1,10 @@
 package com.gazim.gmessenger.api.extensions
 
+import com.gazim.gmessenger.api.wsServer
 import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
 import io.ktor.client.plugins.resources.*
 import io.ktor.client.plugins.websocket.*
-import io.ktor.client.request.*
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
 import io.ktor.client.plugins.websocket.webSocket as webSocketBuilder
@@ -21,12 +21,13 @@ fun HttpClientConfig<*>.configureEngine() =
 
 suspend inline fun <reified T : Any> HttpClient.webSocket(
     resource: T,
-    noinline request: HttpRequestBuilder.() -> Unit = {},
     noinline block: suspend DefaultClientWebSocketSession.() -> Unit,
 ) {
     webSocketBuilder(
-        urlString = href(resource),
-        request = request,
-        block = block,
+        // TODO: HOT FIX
+        urlString = href(resource).let {
+            wsServer.dropLast(1) + it
+        },
+        block = block
     )
 }
